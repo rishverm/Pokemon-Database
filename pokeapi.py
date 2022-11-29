9452147325b5068d4dbbef5aa1eff64685c6f6ca
Avery Feldman <averylf@umich.edu>
2022-11-29 11:55:28 -0500

    started get_move_data(), got the accuracy and name

---
+++ pokeapi.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import os
+import sqlite3
+import random 
+
+def get_move_data():
+    move_list = []
+    move_data = []
+    while len(move_list) <= 24:
+    ## there are a total of 826 moves in pokemon
+        random_num = random.randint(1,826)
+        if random_num not in move_list:
+            move_list.append(random_num)
+            url = "https://pokeapi.co/api/v2/move/" + str(random_num) + "/"
+            r = requests.get(url)
+            content = json.loads(r.text)
+            id = random_num
+            accuracy = content["accuracy"]
+            name = content["name"].split("--")[0]
+            move_data.append((id, name, accuracy))
+
+    print(move_list)
+    print(len(move_list))
+    print(move_data)
+
+get_move_data()
