4b97f25a79675bd810a1f492e577ded91c37b85b
Rishabh Verma <rishverm@umich.edu>
2022-12-03 15:04:10 -0500

    change

--- functions.py
+++ functions.py
@@ -10,19 +10,19 @@ import unicodedata
 class Pokemon:
 
     # TLDR of what has been done: dictionaries of information have been created linking pokemon to their types and moves. A dictionary has also been created that gives more 
-    # information about pokemon moves. I have also created the database declaration with the appropriate tables, and inserted data into Type and moves.
-    # What still has to be done: A dictionary has to be created that connects pokemon to their ability names. We also have to insert data into the Pokemon and Ability table.
+    # information about pokemon moves. A dictionary has also been created that connects pokemon to their ability names. 
+    # I have also created the database declaration with the appropriate tables, and inserted data into all 4 tables.
+    # I believe these are all the functions we need for the python portion of this project but I could be wrong.
     # I understand that this is a lot of changes and new information, but please feel free to change whatever you feel is necessary.
+    # What still has to be done: Process the Data, Visualize the Data, and Report (Part 3-5) 
 
     # initializes database and tables
     def createStructure(self,cur,conn):
-        cur.execute("CREATE TABLE IF NOT EXISTS Pokemon (PokemonID INTEGER PRIMARY KEY, TypeID INTEGER, AbilityIDs LIST, MoveIDs LIST, PokemonName STRING UNIQUE")
+        cur.execute("CREATE TABLE IF NOT EXISTS Pokemon (PokemonID INTEGER PRIMARY KEY, TypeID INTEGER, AbilityIDs CHAR, MoveIDs CHAR, PokemonName STRING UNIQUE)")
         cur.execute("CREATE TABLE IF NOT EXISTS Moves (MoveID INTEGER PRIMARY KEY, TypeID INTEGER, MoveName STRING UNIQUE, Accuracy FLOAT, Power INTEGER, Strength FLOAT)")
         cur.execute("CREATE TABLE IF NOT EXISTS Type (TypeID INTEGER PRIMARY KEY, TypeName STRING UNIQUE)")
         cur.execute("CREATE TABLE IF NOT EXISTS Ability (AbilityID INTEGER PRIMARY KEY, AbilityName STRING UNIQUE)")
         conn.commit()
-
-
 
     #requires limit of number of pokemon
     # chooses the pokemon being focused on, returns a dictionary where the key is pokemon and value are their types
@@ -50,7 +50,6 @@ class Pokemon:
     def getPokemonMoves(self,pokemonDiction):
         pokemonNames= list(pokemonDiction.keys())
         returnedPokemonMoveDiction={}
-        limit=0
         for pokemon in pokemonNames:
             url= "https://pokeapi.co/api/v2/pokemon/" + pokemon.lower() + "/"
             r=requests.get(url)
@@ -63,6 +62,23 @@ class Pokemon:
                 returnedPokemonMoveDiction[pokemon]=movelist[0:15]
                 
         return returnedPokemonMoveDiction
+
+    # requires pokemonDiction returned from getPokemonNameTypes
+    # returns a dictionary where the key is pokemon and value are their list of moves
+    # {pokemon1: [ability1,ability2,ability3], pokemon2: [ability1,ability2,ability3,ability4], pokemon3:[ability1,ability2,ability3,ability4]}
+    def getPokemonAbilities(self,pokemonDiction):
+        pokemonNames= list(pokemonDiction.keys())
+        returnedPokemonAbilityDiction={}
+        for pokemon in pokemonNames:
+            url= "https://pokeapi.co/api/v2/pokemon/" + pokemon.lower() + "/"
+            r=requests.get(url)
+            if r.ok:
+                diction=json.loads(r.text)
+                abilitylist=[]
+                for ability in diction['abilities']:
+                    abilitylist.append(ability['ability']['name'])
+                    returnedPokemonAbilityDiction[pokemon]=abilitylist
+        return returnedPokemonAbilityDiction
 
     # requires pokemonMoveDiction returned from getPokemonMoves
     #returns a dictionary where the key is a pokemon move and value is a dictionary where type,power, accuracy are keys, and values are their actual values
@@ -114,12 +130,7 @@ class Pokemon:
         return returnedMNAPDiction
 
 
-    # need to get abilities info for each pokemon <-pending task
-
-
-
-
-    #inserts data into type table
+    #inserts data into type table using dictionary returned from getPokemonNameTypes (pogoAPI) and getMoveInfo (bulbapedia)
     def insertTypeData(self,cur,conn, pokemonDiction, pokemonMNAPDiction):
         listing=[]
         for pokemon in pokemonDiction:
@@ -132,8 +143,7 @@ class Pokemon:
             cur.execute("INSERT OR IGNORE INTO Type (TypeName) VALUES (?)",(type,))
         conn.commit()
 
-
-    #inserts data into moves table
+    #inserts data into moves table using dictionary returned from getMoveInfo (bulbapedia requirement), as well as Types Table for foreign key
     def insertMoveData(self,cur,conn,pokemonMNAPDiction):
         for moveName, moveVals in pokemonMNAPDiction.items():
             name=moveName
@@ -148,21 +158,74 @@ class Pokemon:
                     break
         conn.commit()
 
+    #inserts data into ability table using dictionary returned from getPokemonAbilities (pokeAPI requirement)
+    def insertAbilityData(self,cur,conn,pokemonAbilityDiction):
+        for abilityList in pokemonAbilityDiction.values():
+            for ability in abilityList:
+                cur.execute("INSERT OR IGNORE INTO Ability (AbilityName) VALUES (?)",(ability,))
+        conn.commit()
 
-
-    # need to add data to Pokemon table, Ability table <-pending task
-
+    #Inserts data into pokemon table using dictionary returned from getPokemonNameTypes (pogoAPI requirement), getPokemonMoves (pokeAPI), and getPokemonAbilities (pokeAPI)
+    #Also uses Type, Moves, and Ability table for foreign keys
+    # cur.execute("CREATE TABLE IF NOT EXISTS Pokemon (PokemonID INTEGER PRIMARY KEY, TypeID INTEGER, AbilityIDs LIST, MoveIDs LIST, PokemonName STRING UNIQUE")
+    
+    def insertPokemonData(self,cur,conn,pokemonDiction, pokemonMoveDiction, pokemonAbilityDiction):
+        for pokemonName,pokemonVals in pokemonDiction.items():
+            try:
+                name=pokemonName
+                type=pokemonVals
+                typeID=0
+                moveNames=pokemonMoveDiction[name]
+                abilityNames=pokemonAbilityDiction[name]
+                moveIDs=[]
+                abilityIDs=[]
+                for move in moveNames:
+                    cur.execute("Select * from Moves")
+                    for row in cur:
+                        if row[2]== move:
+                            moveIDs.append(str(row[0]))
+                            break
+                for ability in abilityNames:
+                    cur.execute("Select * from Ability")
+                    for row in cur:
+                        if row[1]== ability:
+                            abilityIDs.append(str(row[0]))
+                            break
+                cur.execute("Select * from Type")
+                for row in cur:
+                    if row[1]==type:
+                        typeID= int(row[0])
+                        break
+                moveIDs=','.join(moveIDs)
+                abilityIDs=','.join(abilityIDs)
+                cur.execute("INSERT OR IGNORE INTO Pokemon (TypeID,AbilityIDs,MoveIDs,PokemonName) VALUES (?,?,?,?)",(typeID,abilityIDs,moveIDs,name))
+            except:
+                continue
+        conn.commit()
 
 def main():
     conn = sqlite3.connect('PokeDatabase')
     cur=conn.cursor()
     server = Pokemon()
     server.createStructure(cur,conn)
-    pokemonDiction= server.getPokemonNameTypes()
+    print("Create structure has finished")
+    pokemonDiction= server.getPokemonNameTypes(25)
+    print("Pokemon Name Types has finished")
     pokemonMoveDiction=server.getPokemonMoves(pokemonDiction)
+    print("Pokemon moves has finished")
+    pokemonAbilityDiction=server.getPokemonAbilities(pokemonDiction)
+    print("Pokemon Abilities has finished")
     mnapDiction=server.getMoveInfo(pokemonMoveDiction)
+    print("Pokemon move info has finished")
+    # getMoveInfo takes a while (around 120 seconds), the other functions are quick
     server.insertTypeData(cur,conn,pokemonDiction,mnapDiction)
+    print("Type table has finished")
     server.insertMoveData(cur,conn,mnapDiction)
+    print("Move table has finished")
+    server.insertAbilityData(cur,conn,pokemonAbilityDiction)
+    print("Ability table has finished")
+    server.insertPokemonData(cur,conn,pokemonDiction,pokemonMoveDiction,pokemonAbilityDiction)
+    print("Pokemon table has finished")
 
 
 main()
